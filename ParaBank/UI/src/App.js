d572f1c0d8c41fc006269d11cbb9375fb0cf9066
Juan Fe <165521278+juanfedevmaster@users.noreply.github.com>
2026-07-26 18:45:53 -0500

    Add UI App.js

---
+++ ParaBank/UI/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '4rem' }}>
+      <h1>Hello from ParaBank UI!</h1>
+    </div>
+  );
+}
+
+export default App;
