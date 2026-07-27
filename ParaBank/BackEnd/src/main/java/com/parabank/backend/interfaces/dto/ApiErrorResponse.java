e7f7128e1a712206861dde0c6f57eadcb044b30a
Juan Fe <165521278+juanfedevmaster@users.noreply.github.com>
2026-07-26 19:06:56 -0500

    Add ApiErrorResponse DTO

---
+++ ParaBank/BackEnd/src/main/java/com/parabank/backend/interfaces/dto/ApiErrorResponse.java
@@ -0,0 +1,21 @@
+package com.parabank.backend.interfaces.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ApiErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private List<String> details;
+}
